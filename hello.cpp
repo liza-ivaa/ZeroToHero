ff23d2ba9e07c336e0d2ac33dc9031cdd6f0d088
Marysunkas <samsungzhuk7@gmail.com>
2026-02-27 10:25:49 +0200

    Add hello.cpp with a simple greeting program

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Створюємо змінну для числа
+    int chislo; 
+    chislo = 10;
+
+    cout << "Privit! Moie chislo: " << chislo << endl;
+
+    return 0;
+}
