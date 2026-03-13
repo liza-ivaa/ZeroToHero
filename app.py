e142572b5714fc9c6a09279cc064becc2b764521
Liza <yelyzaveta.ivanchenko@lnu.edu.ua>
2026-03-13 11:00:23 +0200

    my first code

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "<h1>Мій проект зі статистики готовий до роботи!</h1>"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        # Отримуємо дані з форми
+        data = request.form.get('numbers')
+        # Перетворюємо рядок у список чисел
+        nums = [float(x.strip()) for x in data.split(',')]
+
+        # Рахуємо статистику
+        result = {
+            "mean": round(np.mean(nums), 2),
+            "median": round(np.median(nums), 2),
+            "var": round(np.var(nums), 2)
+        }
+
+    # render_template шукає файл у папці templates
+    return render_template('index.html', result=result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
